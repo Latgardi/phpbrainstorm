e6adf141a41cb0695db961ec7dc4cf656f5574e6
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 18:49:36 +0300

    add 1.23

---
+++ 1.23.php
@@ -0,0 +1,18 @@
+<?php
+function directOrder($number)
+{
+    $result = 0;
+    for ($i = 0; $i <= 9; $i++) {
+        $a = $number;
+        while ($a > 0) {
+            if ($a % 10 == $i) {
+                $result *= 10;
+                $result += $i;
+            }
+            $a = intdiv($a, 10);
+        }
+    }
+    return $result;
+}
+
+echo directOrder(14278453);
