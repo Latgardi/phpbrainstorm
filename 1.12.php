5230bad0dc65140d5bead17ef5720eb8688fe2d3
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 14:22:12 +0300

    add 1.12

---
+++ 1.12.php
@@ -0,0 +1,15 @@
+<?php
+function isSimple($number)
+{
+    if ($number <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var_dump(isSimple(10006721));
