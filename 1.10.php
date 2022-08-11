960b274115622183741b28822554f77ac98117a8
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 13:30:32 +0300

    add 1.10

---
+++ 1.10.php
@@ -0,0 +1,26 @@
+<?php
+function getReverse(int $number): int
+{
+    $result = 0;
+    while ($number > 0) {
+        $lastNum = $number % 10;
+        $number = intdiv($number, 10);
+        $result += $lastNum;
+        if ($number > 0) {
+            $result *= 10;
+        }
+    }
+    return $result;
+}
+
+function getSqrPalindromes(int $number)
+{
+    for ($i = $number; $i > 0; $i--)
+    {
+        if ($i == getReverse($i) and $i ** 2 == getReverse($i ** 2)) {
+            echo $i . "\n";
+        }
+    }
+}
+
+getSqrPalindromes(100000);
