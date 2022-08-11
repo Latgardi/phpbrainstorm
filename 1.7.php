0ec89655d23cd9a6c95d4209d54af285771f097a
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 11:39:37 +0300

    add 1.6

---
+++ 1.7.php
@@ -0,0 +1,13 @@
+<?php
+function areEqualNumbers($number)
+{
+    for ($number; $number > 0; $number = intdiv($number, 10)) {
+        $lastNum = $number % 10;
+        if ($lastNum == intdiv($number % 100, 10)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var_dump(areEqualNumbers(114567));
