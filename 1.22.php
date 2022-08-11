ee579ff7b370565fdba6ac3610e46a94bcccb1c0
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 18:18:59 +0300

    add 1.22

---
+++ 1.22.php
@@ -0,0 +1,14 @@
+<?php
+
+function getPrimeFactors($number)
+{
+    for ($i = 2; $i <= $number; $i++) {
+        if ($number % $i == 0) {
+            echo "{$i}\n";
+            getPrimeFactors($number / $i);
+            break;
+        }
+    }
+}
+
+getPrimeFactors(1224356);
