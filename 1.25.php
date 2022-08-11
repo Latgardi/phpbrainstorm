e1441cbcd69e82657567b357c2dfe145bb302121
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-11 20:30:34 +0300

    add 1.25

---
+++ 1.25.php
@@ -0,0 +1,29 @@
+<?php
+function factorial($number)
+{
+    if ($number == 0) {
+        return 1;
+    } else {
+        return $number * factorial($number - 1);
+    }
+}
+
+function getFactorionSum($number)
+{
+    $resultSum = 0;
+    $quantity = 0;
+    for ($i = 1; $i <= $number; $i++) {
+        $sum = 0;
+        for ($j = $i; $j > 0; $j = intdiv($j, 10)) {
+            $lastNum = $j % 10;
+            $sum += factorial($lastNum);
+        }
+        if ($i == $sum) {
+            $quantity++;
+            $resultSum += $i;
+        }
+    }
+    return array("sum" => $resultSum, "quantity" => $quantity);
+}
+
+print_r(getFactorionSum(100000));
