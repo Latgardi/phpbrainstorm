14b60062704e39db1af3693d314e48d711aa0bcf
Latgardi <85876063+Latgardi@users.noreply.github.com>
2022-08-17 14:53:02 +0300

    change 1.6

--- 1.6.php
+++ 1.6.php
@@ -1,22 +1,19 @@
 <?php
 function getNumbers()
 {
-    for ($i = 1000; $i <= 9999; $i++) {
-        $o = $i % 10;
-        $d = intdiv($i % 100, 10);
-        $h = intdiv($i % 1000, 100);
-        $t = intdiv($i % 10000, 1000);
-        for ($number = $i; $number > 0; $number = intdiv($number, 10)) {
-            $lastNum = $number % 10;
-            if ($lastNum != 0 and $lastNum != 2 and $lastNum != 3 and $lastNum != 7) {
-                continue 2;
+    for ($i = 1234; $i <= 9999; $i++) {
+        $result = 0;
+        for ($j = 0; $j <= 9; $j++) {
+            for ($number = $i; $number > 0; $number = intdiv($number, 10)) {
+                if ($number % 10 == $j) {
+                    $result *= 10;
+                    $result += $j;
+                }
             }
         }
-        if ($o == $d or $o == $h or $o == $t
-            or $d == $h or $d == $t or $h == $t) {
-            continue;
+        if ($result == 237) {
+            echo $i . "\n";
         }
-        echo $i . "\n";
     }
 }
 
